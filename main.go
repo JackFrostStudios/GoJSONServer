85de30824f1b29caf4084371cc1e36db50c76173
reevesj <jack.reeves@advancedcomputersoftware.com>
2016-03-23 09:43:20 +0000

    Initial Go Server

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type serverResponse struct {
+	TimeDelay int32
+}
+
+var maxDelay int32
+
+func main() {
+	fmt.Printf("Please enter the maximum delay to be tested: ")
+	fmt.Scan(&maxDelay)
+	fmt.Printf("Listening on localhost:9001, will respond after random delay with JSON.\n")
+	rand.Seed(time.Now().Unix())
+	http.HandleFunc("/", returnJSON)         // set router
+	err := http.ListenAndServe(":9001", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
+
+func returnJSON(w http.ResponseWriter, r *http.Request) {
+	timeDelay := rand.Int31n(maxDelay)
+	response := serverResponse{timeDelay}
+	fmt.Printf("Sleeping %d ....\n", timeDelay)
+	time.Sleep(time.Second * time.Duration(timeDelay))
+
+	js, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("Returning %d ....\n", timeDelay)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(js)
+}
